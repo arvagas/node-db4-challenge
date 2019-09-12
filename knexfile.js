d7febfa70ed53b983491f68996f57f6b1ab007af
Arvin Agas <arvagas@gmail.com>
2019-09-12 15:46:41 -0400

    moved location of .db3 file

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './recipe-book.db3'
+      filename: './data/recipe-book.db3'
     },
     useNullAsDefault: true,
     pool: {
