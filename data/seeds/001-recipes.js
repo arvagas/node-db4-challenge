f5fa0bcaf4384abd2badba6090f6fd92c229c55e
Arvin Agas <arvagas@gmail.com>
2019-09-12 14:53:44 -0400

    updated seed data

--- data/seeds/001-recipes.js
+++ data/seeds/001-recipes.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('recipes').truncate()
+  return knex('recipes')
     .then(function () {
       // Inserts seed entries
       return knex('recipes').insert([
         {name: 'Omelet'},
         {name: 'Pizza'},
-        {name: 'Hamburger'}
+        {name: 'Hamburger'},
+        {name: 'Sunny-Side Up Eggs'}
       ]);
     });
 };
